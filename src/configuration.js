6add8c5cf58b0701b646ee822c9673dc85cd905f
Miras Kenzhegaliyev <kenzhemir@gmail.com>
2024-05-02 00:39:08 +0300

    Add read route (#7)
    Signed-off-by: Miras Kenzhegaliyev <kenzhemir@gmail.com>

--- src/configuration.js
+++ src/configuration.js
@@ -3,9 +3,11 @@
  * @typedef Configuration
  * @property {string} baseUrl - This url is going to be used to create the routes
  * @property {Object} [schemas] - Validation schemas
- * @property {Object} [schemas.CreateBody] - Schema for validating
+ * @property {Object} [schemas.CreateBody] - Schema for validating resource for creation
+ * @property {Object} [schemas.IdParam] - Schema for validating :id parameter. Adding validation schema might change the argument type
  * @property {Object} [defaultRouteParams] - Template for creating route
- * @property {(resource) => Promise<void>} [create] - This function will be used to create a resource for POST {baseUrl} calls
+ * @property {(resource) => Promise<void>} [create] - This function will be used to create a resource for POST {baseUrl} route
+ * @property {(resourceId: string | IdParamSchema) => Promise<Resource>} [read] - This function will be used to read a resource for GET {baseUrl}/:id route
  *
  * @param {import('./index').Options} options
  * @returns {{
@@ -18,13 +20,18 @@ export default function createConfiguration(options) {
     return { error: "No baseUrl found" };
   }
 
+  /**
+   * @type Configuration
+   */
   const configuration = {};
   configuration.defaultRouteParams = options.defaultRouteParams;
   configuration.baseUrl = formatBaseUrl(options.baseUrl);
   configuration.create = options.create;
+  configuration.read = options.read;
   if (options.schemas) {
     configuration.schemas = {};
     configuration.schemas.CreateBody = options.schemas.CreateBody;
+    configuration.schemas.IdParam = options.schemas.IdParam;
   }
 
   return { configuration };
---
+++ test/read.test.js
@@ -0,0 +1,95 @@
+import Fastify from "fastify";
+import t from "tap";
+import crudPlugin from "../src/index.js";
+
+// fixtures
+const bookId = 123;
+const bookFromDatabase = {
+  id: bookId,
+  title: "Harry Potter",
+};
+const IdParamSchema = {
+  $id: "IdParamSchema",
+  type: "number",
+};
+
+t.test("Read route is not registered without `read` function", async (t) => {
+  const fastify = Fastify();
+
+  fastify.register(crudPlugin, {
+    baseUrl: "/api/v1/books",
+    // read function is not initialized
+  });
+
+  const fastifyResponse = await fastify.inject({
+    method: "GET",
+    url: `/api/v1/books/${bookId}`,
+  });
+
+  t.equal(fastifyResponse.statusCode, 404);
+
+  const responsePayload = fastifyResponse.json();
+  t.equal(
+    responsePayload.message,
+    `Route GET:/api/v1/books/${bookId} not found`
+  );
+});
+
+t.test("Read route is registered with `read` function", async (t) => {
+  const fastify = Fastify();
+
+  fastify.register(crudPlugin, {
+    baseUrl: "/api/v1/books",
+    read: function readMock(resourceId) {
+      t.strictSame(resourceId, String(bookId));
+      return bookFromDatabase;
+    },
+  });
+
+  const fastifyResponse = await fastify.inject({
+    method: "GET",
+    url: `/api/v1/books/${bookId}`,
+  });
+
+  t.equal(fastifyResponse.statusCode, 200);
+  const responsePayload = fastifyResponse.json();
+  t.strictSame(responsePayload, bookFromDatabase);
+});
+
+t.test("Perform schema validation", async (t) => {
+  const fastify = Fastify();
+
+  fastify.register(crudPlugin, {
+    baseUrl: "/api/v1/books",
+    read: function readMock(resourceId) {
+      t.strictSame(resourceId, bookId);
+      return bookFromDatabase;
+    },
+    schemas: {
+      IdParam: IdParamSchema,
+    },
+  });
+
+  const fastifySuccessResponse = await fastify.inject({
+    method: "GET",
+    url: `/api/v1/books/${bookId}`,
+  });
+
+  t.equal(fastifySuccessResponse.statusCode, 200);
+  const responsePayload = fastifySuccessResponse.json();
+  t.strictSame(responsePayload, bookFromDatabase);
+
+  const fastifyFailuerResponse = await fastify.inject({
+    method: "GET",
+    url: `/api/v1/books/string`,
+  });
+
+  t.equal(fastifyFailuerResponse.statusCode, 400);
+  const failurePayload = fastifyFailuerResponse.json();
+  t.strictSame(failurePayload, {
+    statusCode: 400,
+    code: "FST_ERR_VALIDATION",
+    error: "Bad Request",
+    message: "params/id must be number",
+  });
+});
